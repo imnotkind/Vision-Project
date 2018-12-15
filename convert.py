6153effd4bd10ec26ba96b636d3131cf8089c4cb
haebin307 <billy307@daum.net>
2018-12-15 10:41:49 +0900

    nte

--- convert.py
+++ convert.py
@@ -43,7 +43,7 @@ def level1(video):
                 if predist + 500 < dist:
                     if fadestat < -6:
                         fadeend = count - 1
-                        print("FADE DEC", fadestart, fadeend)
+                        print("################FADE DEC", fadestart, fadeend)
                         fadestart = count - 1
                         fadestat = 0
                     elif fadestat < 0:
@@ -54,7 +54,7 @@ def level1(video):
                 elif predist - 500 > dist:
                     if fadestat > 6:
                         fadeend = count - 1
-                        print("FADE ENC", fadestart, fadeend)
+                        print("################FADE ENC", fadestart, fadeend)
                         fadestart = count - 1
                         fadestat = 0
                     elif fadestat > 0:
@@ -66,11 +66,11 @@ def level1(video):
                 else:
                     if fadestat < -6:
                         fadeend = count - 1
-                        print("FADE DEC", fadestart, fadeend)
+                        print("################FADE DEC", fadestart, fadeend)
                         fadestart = count - 1
                     elif fadestat > 6:
                         fadeend = count - 1
-                        print("FADE ENC", fadestart, fadeend)
+                        print("################FADE ENC", fadestart, fadeend)
                         fadestart = count - 1
 
                     fadestat = 0
