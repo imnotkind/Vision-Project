d1ecbbd4b2f456fa67c59293137fe08c8642e0b4
haebin307 <billy307@daum.net>
2018-12-15 17:54:17 +0900

    bqe
    bv

--- convert.py
+++ convert.py
@@ -3,9 +3,58 @@ import os
 import sys
 from fpdf import FPDF
 import numpy as np
+import mxnet as mx
+from mxnet import image
+from mxnet.gluon.data.vision import transforms
+import gluoncv
+from gluoncv.utils.viz import get_color_pallete
+import matplotlib.image as mpimg
+from PIL import Image
 
-tmp_dir = "./tmp/"
+class Segmentation:
+    def __init__(self, modelname='deeplab_resnet152_voc', palletename='pascal_voc'):
+        self.ctx = mx.gpu(0)
+        self.model = gluoncv.model_zoo.get_model(modelname, pretrained=True, root='.mxnet/models', ctx=self.ctx)
 
+        self.transform_fn = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize([.485, .456, .406], [.229, .224, .225])
+        ])
+        if '_voc' in modelname:
+            self.palletename = 'pascal_voc'
+        elif '_ade' in modelname:
+            self.palletename = 'ade20k'
+        else:
+            print("invalid model name")
+            exit(0)
+
+    def process(self, filename):
+        origimg = image.imread(filename + '.png')
+        img = self.transform_fn(origimg)
+        img = img.expand_dims(0).as_in_context(self.ctx)
+        output = self.model.demo(img)
+        predict = mx.nd.squeeze(mx.nd.argmax(output, 1)).asnumpy()
+
+        s = set([])
+        for (x,y), value in np.ndenumerate(predict):
+            s.add(value)
+        print(s) #0:background, 15:person
+
+        mask = get_color_pallete(predict, self.palletename)
+        mask.save(filename + '_mask.png')
+
+        mask2 = np.array(mask.convert('RGB'))
+
+        overlay = cv2.addWeighted(origimg.asnumpy(),0.5,mask2,0.5,0)
+        overlay = Image.fromarray(overlay)
+        overlay.save(filename + '_overlay.png')
+
+
+
+    def show_img(self, filename):
+        tmp = mpimg.imread(filename)
+        plt.imshow(tmp)
+        plt.show()
 
 def level1(video):
     vidcap = cv2.VideoCapture(video)
@@ -99,7 +148,7 @@ def level1(video):
     for (i,j) in transition2:
         transition3.append((pre, i-1))
         pre = j
-    transition3.append(pre, totalframe-1)
+    transition3.append((pre, totalframe-1))
 
     for j in transition3:
         print(j)
