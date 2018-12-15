6f0a0697e324c5a84b0b90be1f2c54ecd79ba97c
haebin307 <billy307@daum.net>
2018-12-15 11:05:17 +0900

    DIST / PREDIST

--- convert.py
+++ convert.py
@@ -40,7 +40,7 @@ def level1(video):
                 dist = cv2.norm(image, white, cv2.NORM_L2) #white-like measure
                 
 
-                if predist + 1000 < dist:
+                if dist / predist > 1.1:
                     if fadestat < -6:
                         fadeend = count - 1
                         transition.append(("down", fadestart, fadeend))
@@ -51,7 +51,7 @@ def level1(video):
                         fadestat = 0
                     else:
                         fadestat += 1
-                elif predist - 1000 > dist:
+                elif dist / predist < 0.9:
                     if fadestat > 6:
                         fadeend = count - 1
                         transition.append(("up", fadestart, fadeend))
